b31b0a6eef7b22e74c3a16079357964175263909
Leandro Farias <leandrorf1606@gmail.com>
2025-04-28 22:57:47 -0300

    feat: more checkup of routes on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,37 @@ import { NextRequest, NextResponse } from "next/server";
 import { isSessionValid } from "./app/(auth)/_services/session";
 
 export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
-} 
+  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
+};
 
-const publicRoutes = ['/', '/login', '/register', '/post', '/robots', '/sitemap']
+const publicRoutes = [
+  "/",
+  "/login",
+  "/register",
+  "/post",
+  "/robots",
+  "/sitemap",
+];
 
 export async function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname
+  const pathname = req.nextUrl.pathname;
 
   if (publicRoutes.includes(pathname)) {
-    return NextResponse.next()
+    return NextResponse.next();
   }
 
-  if (pathname.startsWith('/post/')) {
-    return NextResponse.next()
+  if (pathname.startsWith("/post/")) {
+    return NextResponse.next();
   }
 
-  const session = await isSessionValid()
+  if (pathname.endsWith('.xml') || pathname.endsWith('.txt')) {
+    return NextResponse.next();
+  }
+  
+  const session = await isSessionValid();
   if (!session) {
-    return NextResponse.redirect(new URL('/login', req.url))
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
